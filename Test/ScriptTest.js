8de1365531de3a71464641de5b63af1324668cb7
MilesTDX <103611277+MilesTDX@users.noreply.github.com>
2022-11-18 16:38:42 -0500

    Add files via upload

---
+++ Test/ScriptTest.js
@@ -0,0 +1,5 @@
+let str = "https://services.pima.gov/SBTDNext/Apps/348/Tickets/Edit?TicketID=594080";
+const myArray = str.split("TicketID=",4);
+let result = myArray[1];
+
+document.getElementsByClassName("demo").innerHTML = result;
