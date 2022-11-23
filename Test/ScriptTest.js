2e0281ebb41984c803e23f191b2c179eae2e8c96
MilesTDX <103611277+MilesTDX@users.noreply.github.com>
2022-11-23 14:08:12 -0500

    Update ScriptTest.js

--- Test/ScriptTest.js
+++ Test/ScriptTest.js
@@ -1,10 +1,11 @@
-function myFunction() {
+function getSource() {
+let formURL = "https://us.ipaas.teamdynamix.com/app/form/start?c=NzU5ZjQxMjUtOTQ3YS00Njc2LTgzYzYtYTU4OWIxN2YzYzU5&t=TWtPZ05RPT14ZmN0Tmw3VEhmSTdBZTNWQWc4OTFtTm5PdjVqajBPb2R5MXlzYmZ4d25qckc2WFYwVmZFYUxCTURhc0x3Vkc5VU5nSDljalZHeHNuZ09WL1JlWkZoSkdRcE54czJCbnEzaTBvK0RuWlFKdmllODBtaXk5TGp1UzcwUjNUL1NlWA&TicketID=";
+
 let str = "https://services.pima.gov/SBTDNext/Apps/348/Tickets/Edit?TicketID=594080";
+
 const myArray = str.split("TicketID=",4);
 let result = myArray[1];
 
-document.getElementsByClassName("demo")[0].innerHTML = result;
+return formURL + result;
+
 }
-
-
-myFunction()
