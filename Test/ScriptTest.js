78bf482cb8d47bdea599453d8475a145db4f17ee
MilesTDX <103611277+MilesTDX@users.noreply.github.com>
2022-11-23 13:15:43 -0500

    Update ScriptTest.js

--- Test/ScriptTest.js
+++ Test/ScriptTest.js
@@ -1,5 +1,7 @@
+function myFunction() {
 let str = "https://services.pima.gov/SBTDNext/Apps/348/Tickets/Edit?TicketID=594080";
 const myArray = str.split("TicketID=",4);
 let result = myArray[1];
 
 document.getElementsByClassName("demo")[0].innerHTML = result;
+}
