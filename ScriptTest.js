c9a834f035231c07bd702266fa1bf81cc9611017
MilesTDX <103611277+MilesTDX@users.noreply.github.com>
2023-01-20 11:12:27 -0500

    Update ScriptTest.js

--- ScriptTest.js
+++ ScriptTest.js
@@ -1,5 +1,3 @@
-let str = "https://services.pima.gov/SBTDNext/Apps/348/Tickets/Edit?TicketID=594080";
-const myArray = str.split("TicketID=",4);
-let result = myArray[1];
-
-document.getElementsByClassName("demo").innerHTML = result;
+if ("{{StepType}}" == "Approval") {
+document.getElementById("p1").innerHTML = "{{StepType}}";
+}
