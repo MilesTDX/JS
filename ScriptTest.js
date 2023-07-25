702ab1fa9aa4ddfd70291bec1f440bc261401323
MilesTDX <103611277+MilesTDX@users.noreply.github.com>
2023-07-25 08:41:46 -0400

    Update ScriptTest.js

--- ScriptTest.js
+++ ScriptTest.js
@@ -1,3 +1,21 @@
-if ("{{StepType}}" == "Approval") {
-document.getElementById("p1").innerHTML = "{{StepType}}";
+var styles = `
+h1 { 
+  display: block;
+  font-size: 5em;
+  margin-top: 0.67em;
+  margin-bottom: 0.67em;
+  margin-left: 0;
+  margin-right: 0;
+  font-weight: bold;
 }
+
+button { 
+  display: block;
+  font-size: 5em;
+  font-weight: bold;
+}
+`
+
+var styleSheet = document.createElement("style")
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
